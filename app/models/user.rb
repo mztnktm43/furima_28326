7c1d0978a4355647abddf7f951b4c9865d6a6770
mztnktm43 <spngkob.714bd@gmail.com>
2020-08-16 14:26:12 +0900

    バリデーション&アソシエーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :items
-  has_many :orders
+    has_many :items
+    has_many :orders
+
+  with_options presences: true do
+    validates :nickname 
+    validates :email, format: {with: /@+/ }#一意性(カラム内での重複禁止)、＠を含む
+    validates :encrypted_password, format: { with: /\d{6}/, PASSWORD_REGEX }#６文字以上、半角英数混合
+    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }#全角ひらがなカタカナ漢字
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }# 〃
+    validates :family_name_reading, format: { with: /\A[ァ-ヶー－]+\z/ }#全角カタカナ
+    validates :first_name_reading, format: { with: /\A[ァ-ヶー－]+\z/ }# 〃
+    validates :birthday
+  end
 end
