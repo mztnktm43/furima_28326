6f7cbd01c452865aff84deda824de502ab7fa31b
mztnktm43 <spngkob.714bd@gmail.com>
2020-08-18 18:47:37 +0900

    new,createアクションのルーティング設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,18 @@
 class ItemsController < ApplicationController
   def index
+
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+
+  private
+
+  def item_params
+    params.require(:tweet).permit(:name, :comment, :cost, :category_id, :product_status_id, :fee_id, :origin_prefecture_id, :delivery_day_id)
   end
 end
