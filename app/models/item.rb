f909ba06654124d9f7efc3ad09ddf90907bf980c
mztnktm43 <spngkob.714bd@gmail.com>
2020-08-18 18:13:16 +0900

    itemsテーブルのバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,17 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :user
+  has_one_attached :image
+  has_one :order
+  belongs_to_active_hash :category
+  belongs_to_active_hash :product_status
+  belongs_to_active_hash :fee
+  belongs_to_active_hash :origin_prefecture
+  belongs_to_active_hash :delivery_day
+
+  with_options presence: true do 
+  end 
+
+  validates :genre_id, numericality: { other_than: 1 }
+  #ActiveHashが「--」のときに保存できない
 end
