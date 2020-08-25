b3b022e54d927a42fdcd3e0354e5a18b831a540c
mztnktm43 <spngkob.714bd@gmail.com>
2020-08-25 14:39:01 +0900

    購入画面への遷移

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,21 @@
 class OrdersController < ApplicationController
+  before_action :move_to_new, only: [:index, :create, :new]
+
+  def index
+    @item = Item.find(params[:item_id])
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  private
+
+  def move_to_new
+    unless user_signed_in?
+      redirect_to user_session_path 
+    end
+  end
 end
